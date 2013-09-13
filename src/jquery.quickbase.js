bc88a2d9c582a772ee44788418090bb7e5531793
Joshua McGinnis <joshua@mcginn.is>
2013-09-12 17:27:28 -0700

    Refactor build_schema to be fully asynchronous.

--- src/jquery.quickbase.js
+++ src/jquery.quickbase.js
@@ -1,222 +1,268 @@
 (function($) {
-    $.QuickBase = function(options) {
-        var defaults = {
-            username: '',
-            password: '',
-            dbid: '',
-            apptoken: '',
-            realm: 'www.quickbase.com'      
-        }
+	$.QuickBase = function(options) {
+		var defaults = {
+			username: '',
+			password: '',
+			dbid: '',
+			apptoken: '',
+			realm: 'www.quickbase.com'      
+		}
 
-        var plugin = this;
+		var plugin = this;
 
-        plugin.settings = {};
-        plugin.payload = $('</qbdapi>');
+		plugin.settings = {};
+		plugin.payload = $('</qbdapi>');
 
-        plugin.init = function(callback) {
-            plugin.settings = $.extend({}, defaults, options);
+		plugin.init = function(callback) {
+			plugin.settings = $.extend({}, defaults, options);
 
-            plugin.base_url = 'https://' + plugin.settings.realm + '/db/';
+			plugin.base_url = 'https://' + plugin.settings.realm + '/db/';
 
-            if(typeof plugin.token === 'undefined') {
-                plugin.authenticate(undefined, undefined, undefined, function(response) {
-                    plugin.ticket = $(response).find('ticket').text();
+			if(typeof plugin.token === 'undefined') {
+				plugin.authenticate(undefined, undefined, undefined, function(response) {
+					plugin.ticket = $(response).find('ticket').text();
 
-                    return typeof(callback) == "function" ? callback() : this;
-                });
-            }
-            else
-                return typeof(callback) == "function" ? callback() : this;
-        }
+					return typeof(callback) == "function" ? callback() : this;
+				});
+			}
+			else
+				return typeof(callback) == "function" ? callback() : this;
+		}
 
-        $.QuickBase.record = function(o) {
-            var record = $.extend({
-                fid: '',
-                name: '',
-                value: '',
-            }, o);
+		plugin.record = function(fid, value) {
+			var id = isNaN(fid) ? 'name="' + fid + '"' : 'fid="' + fid + '"';
 
-            try {
-                if(record.fid === '' && record.name === '')
-                    throw "Missing record fid or name.";
+			return '<field ' + id + '>' + value + '</field>';
+		}
 
-                var id = record.fid !== '' ? 'fid="' + record.fid + '"' : 'name="' + record.name + '"';
+		function reset_payload() {
+			plugin.payload = $('<qdbapi>');
+			plugin.payload.append($('<ticket>').text(plugin.ticket));
+			plugin.payload.append($('<apptoken>').text(plugin.settings.apptoken));        
+		}
 
-                return '<field ' + id + '>' + record.value + '</field>';
-            } catch (e) {
-                handleErrors(e);
-            }
-        }
+		function get_raw_xml(payload) {
+			return $('<div/>').append(payload.clone()).html();
+		}
 
-        function reset_payload() {
-            plugin.payload = $('<qdbapi>');
-            plugin.payload.append($('<ticket>').text(plugin.ticket));
-            plugin.payload.append($('<apptoken>').text(plugin.settings.apptoken));        
-        }
+		function QuickBaseException(action, errcode, errtext, errdetail) {
+			this.action = action;
+			this.errcode = errcode;
+			this.errtext = errtext;
+			this.errdetail = errdetail;
+		}
 
-        function get_raw_xml(payload) {
-            return $('<div/>').append(payload.clone()).html();
-        }
+		function handleErrors(e) {
+			console.log(e.message);
+		}
 
-        function QuickBaseException(action, errcode, errtext, errdetail) {
-            this.action = action;
-            this.errcode = errcode;
-            this.errtext = errtext;
-            this.errdetail = errdetail;
-        }
+		function transmit(action, payload, url, callback) {
+			var url = (url === 'main') ? 
+					plugin.base_url + 'main' : 
+					plugin.base_url + plugin.settings.dbid;
 
-        function handleErrors(e) {
-            console.log(e);
-        }
+			$.ajax({
+				type: 'POST',
+				beforeSend: function(request) {
+				  request.setRequestHeader('QUICKBASE-ACTION', action);
+				},
+				url: url,
+				data: get_raw_xml(payload),
+				dataType: 'xml',
+				contentType: 'text/xml',
+				success: function(data, textStatus, jqXHR) {
+					var errcode = parseInt($(data).find('errcode').text());
 
-        function transmit(action, payload, url, callback) {
-            var url = (url === 'main') ? 
-                    plugin.base_url + 'main' : 
-                    plugin.base_url + plugin.settings.dbid;
+					try {
+						if(errcode > 0) {
+							var errtext = $(data).find('errtext').text(),
+								errdetail = $(data).find('errdetail').text();
 
-            $.ajax({
-                type: 'POST',
-                beforeSend: function(request) {
-                  request.setRequestHeader('QUICKBASE-ACTION', action);
-                },
-                url: url,
-                data: get_raw_xml(payload),
-                dataType: 'xml',
-                contentType: 'text/xml',
-                success: function(data, textStatus, jqXHR) {
-                    var errcode = parseInt($(data).find('errcode').text());
+							throw new QuickBaseException(action, errcode, 
+									errtext, errdetail);
+						}
 
-                    try {
-                        if(errcode > 0) {
-                            var errtext = $(data).find('errtext').text(),
-                                errdetail = $(data).find('errdetail').text();
+						reset_payload();
 
-                            throw new QuickBaseException(action, errcode, 
-                                    errtext, errdetail);
-                    }
+						return typeof(callback) == "function" ? callback(data) : data;
 
-                    reset_payload();
+				  } catch (e) {
+					handleErrors(e);
+				  }
+				},
+				error: function(xhr, ajaxOptions, thrownError) {
+					console.log(xhr);
+					console.log(ajaxOptions);
+					console.log(thrownError);
+				}
+			});
+		}
 
-                    return typeof(callback) == "function" ? callback(data) : data;
+		plugin.authenticate = function(username, password, hours, callback) {
+			var payload = $('<qdbapi>');
 
-                  } catch (e) {
-                    handleErrors(e);
-                  }
-                },
-                error: function(xhr, ajaxOptions, thrownError) {
-                    console.log(xhr);
-                    console.log(ajaxOptions);
-                    console.log(thrownError);
-                }
-            });
-        }
+			if(typeof username === 'undefined')
+				username = plugin.settings.username;
 
-        plugin.authenticate = function(username, password, hours, callback) {
-            var payload = $('<qdbapi>');
+			if(typeof password === 'undefined')
+				password = plugin.settings.password;
 
-            if(typeof username === 'undefined')
-                username = plugin.settings.username;
+			if(typeof hours === 'undefined')
+				hours = ''; // use QB default of 12 hours
 
-            if(typeof password === 'undefined')
-                password = plugin.settings.password;
+			payload.append($('<username>').text(username));
+			payload.append($('<password>').text(password));
+			payload.append($('<hours>').text(hours));
 
-            if(typeof hours === 'undefined')
-                hours = ''; // use QB default of 12 hours
+			transmit('API_AUTHENTICATE', payload, 'main', callback);
+		}
 
-            payload.append($('<username>').text(username));
-            payload.append($('<password>').text(password));
-            payload.append($('<hours>').text(hours));
+		plugin.build_schema = function(callback) {
+			var schema = {};
 
-            transmit('API_AUTHENTICATE', payload, 'main', callback);
-        }
+			transmit('API_GetSchema', plugin.payload, 'db', function(data) {
 
-        plugin.build_schema = function(callback) {
-            transmit('API_GetSchema', plugin.payload, 'db', function(data) {
+				try {
+					var $chdbids = $(data).find('chdbid');
+					if ($chdbids.length === 0)
+						throw 'This operation requires a parent-level DBID.';
+				}
+				catch (e) {
+					handleErrors(e);
+				}
+				
+				function build_schema_obj(index) {
 
-                try {
-                    var $chdbids = $(data).find('chdbid');
-                    if ($chdbids.length === 0)
-                        throw 'This operation requires a parent-level DBID.';
-                }
-                catch (e) {
-                    handleErrors(e);
-                }
+					plugin.settings.dbid = $($chdbids[index]).text();
 
-                var schema = {};
+					if(index+1 > $chdbids.length) {
 
-                $chdbids.each(function(index, value) {
-                    var dbid = $(value).text();
-                    var table_name = $(value).attr('name').substring('_dbid_'.length);
+						return typeof(callback) == "function" ? callback( schema ) : schema;
 
-                    schema[table_name] = {'dbid': dbid, fields: {}, queries: {}};
+					} else {
+						plugin.get_schema(function(data) {
+							var $chdbid = $($chdbids[index]),
+								   dbid = $chdbid.text(),
+							 table_name = $chdbid.attr('name').substring('_dbid_'.length);
 
-                    plugin.settings.dbid = dbid;
+							schema[table_name] = {'dbid': dbid};
 
-                    plugin.get_schema(function(data) {
-                        var $fields = $(data).find('field');
+							function build_fields_obj(i) {
+								i = i || 0;
 
-                        $fields.each(function(index, value) {
-                            var field_label_raw = $(value).find('label').text()
-                            var field_label = field_label_raw.toLowerCase().replace(/\W/g, '_');
-                            var field_id = $(value).attr('id');
-                            var field_type = $(value).attr('field_type');
+								if(i+1 > $fields.length) {
+									$.extend(schema[table_name], f);
 
-                            var f = {};
-                            f[field_label] = {id: field_id, label: field_label_raw, 'field_type': field_type};
-                            $.extend(schema[table_name].fields, f);
-                        });
+									return false;
+								} else {
+									var $field = $($fields[i]),
+										field_label_raw = $field.find('label').text(),
+										field_label = field_label_raw.toLowerCase().replace(/\W/g, '_'),
+										field_id = $field.attr('id'),
+										field_type = $field.attr('field_type');
 
-                        var $queries = $(data).find('query');
+									f.fields[field_label] = {id: field_id, label: field_label_raw, 'field_type': field_type};
 
-                        $queries.each(function(index, value) {
-                            var query_id = $(value).attr('id');
-                            var query_name_raw = $(value).find('qyname').text();
-                            var query_name = query_name_raw.toLowerCase().replace(/\W/g, '_');
+									build_fields_obj(i+1);									
+								}
+							}
 
-                            var q = {};
-                            q[query_name] = {id: query_id, name: query_name_raw};
-                            $.extend(schema[table_name].queries, q);
-                        });
-                    });
-                });
+							function build_queries_obj(i) {
+								i = i || 0;
 
-                return typeof(callback) == "function" ? callback(schema) : schema;
-            });  
-        }
+								if(i+1 > $queries.length) {
+									$.extend(schema[table_name], q);
 
-        plugin.get_schema = function(callback) {
-            transmit('API_GetSchema', plugin.payload, 'db', function(data) {
-                return typeof(callback) == "function" ? callback(data) : data;
-            });
-        }
+									return false;
+								} else {
 
-        plugin.add_record = function(callback) {
+									var $query = $($queries[i]);
+										query_id = $query.attr('id'),
+										query_name_raw = $query.find('qyname').text(),
+										query_name = query_name_raw.toLowerCase().replace(/\W/g, '_');
 
-        }
+									q.queries[query_name] = {id: query_id, name: query_name_raw};
 
-        plugin.edit_record = function(callback) {
-            
-        }
+									build_queries_obj(i+1);									
+								}
+							}
 
-        plugin.do_query = function(query, options, callback) {
-            var q = $('<qid>');
+							var $fields = $(data).find('field'),
+									  f = {'fields': {}};
 
-            if(isNaN(query)) {
-                q = Boolean(query.match('^[{]')) ? $('<query>') : $('<qname>');
-            }
+							if($fields.length) {
+								build_fields_obj();
+							}
 
-            plugin.payload.append(q.text(query));
+							var $queries = $(data).find('query'),
+								q = {'queries': {}};
 
-            for (var opt in options) {
-                var opt_elem = '<' + opt + '>';
-                plugin.payload.append($(opt_elem).text(options[opt]));
-            }
+							if($queries.length) {
+								build_queries_obj();
+							}
 
-            transmit('API_DoQuery', plugin.payload, 'db', function(data) {
-                return typeof(callback) == "function" ? callback(data) : data;
-            });
-        }
+							build_schema_obj(index+1);
+						});
+					}
+				}
 
-        return this;
+				return build_schema_obj(0);
+			});  
+		}
+
+		plugin.get_schema = function(callback) {
+			transmit('API_GetSchema', plugin.payload, 'db', function(data) {
+				return typeof(callback) == "function" ? callback(data) : data;
+			});
+		}
+
+		plugin.add_record = function(records, options, callback) {
+			if ( typeof(options) == "function") {
+				callback = options;
+				options = undefined;
+			}
+
+			if(options) {
+				$.each(options, function(index, value) {
+					plugin.payload.append($(value));
+				});
+			}
+
+			$.each(records, function(index, value) {
+				plugin.payload.append($(value));
+			});
+
+			transmit('API_AddRecord', plugin.payload, 'db', function(data) {
+			    return typeof(callback) == "function" ? callback(data) : data;
+			});
+		}
+
+		plugin.edit_record = function(callback) {
+			// transmit('API_EditRecord', plugin.payload, 'db', function(data) {
+			//     return typeof(callback) == "function" ? callback(data) : data;
+			// });
+		}
+
+		plugin.do_query = function(query, options, callback) {
+
+			var q = $('<qid>');
+
+			if(isNaN(query)) {
+				q = Boolean(query.match('^[{]')) ? $('<query>') : $('<qname>');
+			}
+
+			plugin.payload.append(q.text(query));
+
+			for (var opt in options) {
+				var opt_elem = '<' + opt + '>';
+				plugin.payload.append($(opt_elem).text(options[opt]));
+			}
+
+			transmit('API_DoQuery', plugin.payload, 'db', function(data) {
+				return typeof(callback) == "function" ? callback(data) : data;
+			});
+		}
+
+		return this;
   }
 }(jQuery));
